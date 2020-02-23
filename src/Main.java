5e5bb4c8bbed6b7eeb3181099c9e1d258ed9a32a
Musppelheim <neloliver@hotmail.com>
2020-02-23 20:18:15 +0700

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        boolean registered = true;
         int payment = 1050;
-        //пополнение баланса
-        int balance = 17;
-        double v = 1.1;
-        double cashback;
-        // cashback: по 1 рублю за каждые 100 рублей
-        cashback = 1.1;
-        int account = (int) (payment * cashback) + balance;
-        if (payment * cashback > 100) {
-            System.out.println(account);
-        }
+        int bonus = payment / 100;
+        int total = payment + bonus;
+        if (payment > 1000)
+        System.out.println(total);
+
     }
 }
 
