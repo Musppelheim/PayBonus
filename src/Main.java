1de82392ecd429c678a4aab7ccdaa96802f5039e
Musppelheim <neloliver@hotmail.com>
2020-02-23 22:37:29 +0700

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        boolean registered = true;
+        boolean condition = true;
+        int balance = 25;
         int payment = 1050;
         int bonus = payment / 100;
-        int total = payment + bonus;
+        int total = payment + balance + bonus;
         if (payment > 1000)
         System.out.println(total);
 
