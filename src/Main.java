d5a8a2e0aade9de017c04f69eb0058664798bc8c
Musppelheim <neloliver@hotmail.com>
2020-02-26 08:50:59 +0700

    bonus added

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ public class Main {
     public static void main(String[] args) {
         boolean condition = true;
         int balance = 25;
-        int payment = 1050;
+        int payment = 1250;
         int bonus = payment / 100;
-        int total = payment + balance + bonus;
-        if (payment > 1000)
-        System.out.println(total);
-
+        int total = balance + payment + bonus;
+        if (payment > 1000) {
+            bonus = payment /100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
     }
 }
 
